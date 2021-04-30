3be8853eea2ffe0e8d3d46e70013485f42c9c1de
0xffset <rene.gottschalk02@gmail.com>
2021-04-30 23:10:47 +0200

    init

---
+++ public/js/app.js
@@ -0,0 +1,31 @@
+function submitButtonClick() {
+    let url = document.getElementById("url").value.trim();
+
+    if (!url.startsWith("http://") && !url.startsWith("https://")) {
+        url = "https://" + url;
+    }
+
+    fetch("shorten", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "url": url
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("title").innerText = data.shortUrl;
+            document.getElementById("title").classList.add("copy-title");
+        });
+}
+
+function copyTitle() {
+    const el = document.createElement('textarea');
+    el.value = document.getElementById("title").innerText;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+}
